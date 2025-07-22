481e92ae6544e3c7f9b1db0cb275a10b5932867c
nasiralibaloch <nasiralibaloch110@gmail.com>
2025-07-22 11:10:46 +0500

    learning all about vue js

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Hero from "./components/Hero.vue";
+import HomeCards from "./components/HomeCards.vue";
+import JobListings from "./components/JobListings.vue";
+
+import NavBar from "./components/NavBar.vue";
+</script>
+<template>
+  <NavBar />
+  <Hero />
+  <HomeCards/>
+  <JobListings :limit="3" :showButton="true"/>
+ 
+</template>
